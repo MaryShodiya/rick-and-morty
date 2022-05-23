ddd5e97dec4aa214fbbe2851cf1f1bcb31af911a
MaryShodiya <marysho57@gmail.com>
2022-05-23 15:40:26 +0100

    added a read me file and a seperatee html,css and js file to do some testings

---
+++ READ ME.md
@@ -0,0 +1,38 @@
+# Adventures With Rick and MOrty
+
+Adventures with Rick and Morty is a full-stack web application that takes you on an adventure with the entire rick and morty team. Data was collected from the [rick and morty api](https://rickandmortyapi.com)
+
+[Tap to access the project here](https://adventure-with-the-rick-and-morty-team.netlify.app)
+
+<img align="center" width="300" src="img/rick.gif" alt="rick and morty" />
+
+### The Process
+
+Tech Used: HTML, CSS, JavaScript, REST API
+
+An app that gives rick and morty lovers a complete adventure with the entire rick and morty team
+## Optimization
+This project uses the click event to handle navigation through the page
+
+## Lessons Learnt
+
+Spent more hours in figuring how to get the entire data from the page as the api was divided into 42(forty-two) pages as against previous experience of working with an api with a single page. I had to create an empty array, loop through the entire 42 pages, then push the data to the empty array. 
+# Syntax
+
+```
+let currentPages = []
+
+document.querySelector('#get-btn').addEventListener('click', getButton)
+function getButton() {
+for (let i = 0; i<= 42; i++){
+    fetch(url+i)
+    .then(res => res.json()) 
+    .then(data => {
+    currentPages.push(data.results)
+```
+
+
+## Resources Used
+Stack Overflow
+WW3 schools
+
---
+++ main2.js
@@ -0,0 +1,56 @@
+
+let url= "https://rickandmortyapi.com/api/character?page="
+let currentPages = []
+let page = 1
+let index = 0
+document.getElementById('welcome').addEventListener('click',()=>{
+    document.querySelector('.story').style.display='inline'
+    document.querySelector('#get-btn').style.display='block'
+    document.getElementById('welcome').style.display='none'
+})
+
+document.querySelector('#get-btn').addEventListener('click', getButton)
+function getButton() {
+for (let i = 0; i<= 42; i++){
+    fetch(url+i)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+    currentPages.push(data.results)
+    document.querySelector('.story').style.display='none'
+  document.querySelector('.box').style.display='block'
+document.querySelector('img').style.display='block'
+document.querySelector('#next-page').style.display='block'
+ document.querySelector('.name').innerText = currentPages[page][index].name
+ document.querySelector('img').src= currentPages[page][index].image
+ document.querySelector('.species').innerText = currentPages[page][index].species
+ document.querySelector('.status').innerText = currentPages[page][index]. status
+ document.querySelector('.location').innerText = currentPages[page][index].location.name
+ document.querySelector('#get-btn').style.display='none'
+
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+}
+
+
+
+ document.querySelector('.fa-hand-point-right').addEventListener('click', nextButton)
+function nextButton(){
+    currentPages[index++]
+    getButton()
+}
+
+document.querySelector('#next-page').addEventListener('click', nextPage)
+function nextPage(){
+    currentPages[page++]
+    getButton()
+    
+}
+
+document.querySelector('.fa-hand-point-left').addEventListener('click', prevButton)
+function prevButton(){
+    currentPages[index--]
+    getButton()
+}
