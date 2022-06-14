b78cee8006d40e632deb0fc9e7c8a83f86f5f00a
MaryShodiya <marysho57@gmail.com>
2022-06-14 17:02:07 +0100

    added volume buttons and icons

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-
-let url= "https://rickandmortyapi.com/api/character?page="
-let currentPages = []
+//url address
+let url= "https://rickandmortyapi.com/api/character?page=" 
+//empty array
+let currentPages = [] 
+//current number of pages
 let page = 1
+//index num
 let index = 0
+
+//define the span icon of volume
+let volumeOn = document.querySelector('.fa-volume-up')
+let volumeOff = document.querySelector('.fa-volume-off')
+
+//event listener for welcome prompt
 document.getElementById('welcome').addEventListener('click',()=>{
     document.querySelector('.story').style.display='inline'
     document.querySelector('#get-btn').style.display='block'
     document.getElementById('welcome').style.display='none'
 })
 
+//event listener for main page that displays data from array
 document.querySelector('#get-btn').addEventListener('click', getButton)
 function getButton() {
+    //loops through the 42 pages of datas in the api
 for (let i = 0; i<= 42; i++){
+    //fetches the url address and the iterated 42 pages of data
     fetch(url+i)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-       document.querySelector('audio').play()
+
     currentPages.push(data.results)
     document.querySelector('.story').style.display='none'
   document.querySelector('.box').style.display='block'
@@ -27,6 +39,19 @@ document.querySelector('#next-page').style.display='block'
  document.querySelector('.status').innerText = currentPages[page][index]. status
  document.querySelector('.location').innerText = currentPages[page][index].location.name
  document.querySelector('#get-btn').style.display='none'
+
+//to play music
+ volumeOn.addEventListener('click',musicOn)
+ function musicOn() {
+    document.querySelector('audio').play()
+ }
+
+ //to off music
+ volumeOff.addEventListener('click', musicOff)
+ function musicOff() {
+    document.querySelector('audio').pause()
+ }
+
     })
     .catch(err => {
         console.log(`error ${err}`)
